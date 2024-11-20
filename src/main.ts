4e84a39f2a7bd363e9c11302e6345120337d1139
pacexy <pacexy@gmail.com>
2024-11-20 18:03:55 +0800

    feat: ui for file size filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,42 @@
 import { filterSize } from './filters/size'
+import './main.css'
 
 const trs = document.querySelectorAll<HTMLTableRowElement>(
   'table.torrent-list tr',
 )
 
-Array.from(trs)
-  .filter((tr) => !filterSize(tr, { min: 1024 }))
-  .forEach((tr) => {
-    tr.style.opacity = '0.5'
-  })
+const container = document.querySelector<HTMLDivElement>('body > div.container')
+
+if (container) {
+  const root = document.createElement('div')
+  root.id = 'nyaa-filters'
+  container.prepend(root)
+
+  const label = document.createElement('label')
+  label.textContent = 'File Size:'
+  root.appendChild(label)
+
+  const minInput = document.createElement('input')
+  minInput.type = 'number'
+  minInput.placeholder = 'min'
+  root.appendChild(minInput)
+
+  const maxInput = document.createElement('input')
+  maxInput.type = 'number'
+  maxInput.placeholder = 'max'
+  root.appendChild(maxInput)
+
+  const onInput = () => {
+    const minSize = parseFloat(minInput.value) || undefined
+    const maxSize = parseFloat(maxInput.value) || undefined
+
+    Array.from(trs)
+      .filter((tr) => !filterSize(tr, { min: minSize, max: maxSize }))
+      .forEach((tr) => {
+        tr.style.opacity = '0.5'
+      })
+  }
+
+  minInput.addEventListener('input', onInput)
+  maxInput.addEventListener('input', onInput)
+}
